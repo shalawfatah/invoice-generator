9a1937821fc9f2a270c8ffab7a9382867a0bcd37
Shalaw Fatah <shalaw@glaciermediadigital.ca>
2023-08-22 02:42:03 -0700

    stripe pk

--- App.tsx
+++ App.tsx
@@ -18,23 +18,25 @@ export default function App() {
     supabase.auth.getSession().then(({ data: { session } }) => {
       setSession(session)
       setUser(session.user)
-      setIsSessionSet(true)
+      setIsSessionSet(true) // Mark session as set
     })
     supabase.auth.onAuthStateChange((_event, session) => {
       setSession(session)
       setUser(session.user)
-      setIsSessionSet(true)
+      setIsSessionSet(true) // Mark session as set
     })
   }, [])
 
   useEffect(() => {
     if (isSessionSet) {
-      setUser(session.user)
+      // Now, you can set the user state after the session state is set
+      // This will ensure that the user state is only set after session state is available
+      // Do any additional processing or rendering that involves both session and user here
     }
   }, [isSessionSet])
 
   return (
-    <StripeProvider publishableKey='pk_test_...'>
+    <StripeProvider publishableKey='pk_test_51HhPuUK1omnuMJYrNMXx9TZgm4MhtRewC2QU8WTBBcHUTBUFshVHc3HCI7xXoPwjUC8asvr03tz9hLOWBRUXasSy00uLZxpufi'>
       <SessionContext.Provider value={user}>
         {isSessionSet && session && session.user ? <Navigation /> : <Auth />}
       </SessionContext.Provider>
