beb6cff14ed75f185adcba1db12f391bb99aef75
Shalaw Fatah <shalaw.fatah@gmail.com>
2023-11-03 06:08:45 -0700

    eas update enabled for faster updates

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
 import { Provider } from 'jotai'
 import Main from './pages/Main'
+import * as Updates from "expo-updates"
+import { useEffect } from 'react';
 
 export default function App() {
+  async function onFetchUpdateAsync() {
+    try {
+      const update = await Updates.checkForUpdateAsync();
 
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  }
+
+  useEffect(() => {
+    onFetchUpdateAsync()
+  }, [])
   return (
     <Provider>
       <Main />
